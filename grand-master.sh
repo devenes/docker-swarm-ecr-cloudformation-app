a5014ef97cbc972ca10b0d722e02d842b3dc847f
Enes <66560757+devenes@users.noreply.github.com>
2022-05-06 02:39:36 +0300

    Update grand-master.sh

--- grand-master.sh
+++ grand-master.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 yum update -y
-hostnamectl set-hostname Leader-Manager
+hostnamectl set-hostname Grand-Master
 amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
@@ -9,7 +9,7 @@ curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compo
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker swarm init
-aws ecr get-login-password --region ${data.aws_region.current.name} | docker login --username AWS --password-stdin ${aws_ecr_repository.ecr-repo.repository_url}
+aws ecr get-login-password --region ${AWS::Region} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
 docker service create \
     --name=viz \
     --publish=8080:8080/tcp \
@@ -22,10 +22,23 @@ rm -rf /bin/aws
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
-docker build --force-rm -t "${aws_ecr_repository.ecr-repo.repository_url}:latest" ${local.github-repo}
-docker push "${aws_ecr_repository.ecr-repo.repository_url}:latest"
+yum install amazon-ecr-credential-helper -y
+mkdir -p /home/ec2-user/.docker
+cd /home/ec2-user/.docker
+echo '{"credsStore": "ecr-login"}' > config.json
+aws ecr create-repository \
+    --repository-name ${APP_REPO_NAME} \
+    --image-scanning-configuration scanOnPush=false \
+    --image-tag-mutability MUTABLE \
+    --region ${AWS::Region}
+docker build --force-rm -t "${ECR_REGISTRY}/${APP_REPO_NAME}:latest" ${GITHUB_REPO}
+docker push "${ECR_REGISTRY}/${APP_REPO_NAME}:latest"
 mkdir -p /home/ec2-user/phonebook
-cd /home/ec2-user/phonebook && echo "ECR_REPO=${aws_ecr_repository.ecr-repo.repository_url}" > .env
-curl -o "docker-compose.yml" -L ${local.github-file-url}docker-compose.yml
-curl -o "init.sql" -L ${local.github-file-url}init.sql
+cd /home/ec2-user/phonebook
+cat << EOF | tee .env
+ECR_REGISTRY=${ECR_REGISTRY}
+APP_REPO_NAME=${APP_REPO_NAME}
+EOF
+curl -o "docker-compose.yml" -L ${GIT_FILE_URL}docker-compose.yml
+curl -o "init.sql" -L ${GIT_FILE_URL}init.sql
 docker-compose config | docker stack deploy --with-registry-auth -c - phonebook
